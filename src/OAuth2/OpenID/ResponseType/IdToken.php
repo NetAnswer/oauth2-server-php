0ac07ae647b6d0bb4a9cd6f46fc8268958b7efd8
netanswer-devs <46164985+netanswer-devs@users.noreply.github.com>
2021-01-18 20:16:29 +0100

    Update IdToken.php

--- src/OAuth2/OpenID/ResponseType/IdToken.php
+++ src/OAuth2/OpenID/ResponseType/IdToken.php
@@ -97,6 +97,7 @@ class IdToken implements IdTokenInterface
         $token = array(
             'iss'        => $this->config['issuer'],
             'sub'        => $user_id,
+            'user_id'    => $user_id,
             'aud'        => $client_id,
             'iat'        => time(),
             'exp'        => time() + $this->config['id_lifetime'],
